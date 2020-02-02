d19b10c78b16212b33250721fe9543acbd637fab
pkmm <robotgg@126.com>
2020-02-02 11:25:44 +0800

    调整中间件代码

--- middleware/middleware_error_handler.go
+++ middleware/middleware_error_handler.go
@@ -5,25 +5,21 @@ import (
 	"cgin/errno"
 	"cgin/service"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func BusinessErrorHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		defer func() {
 			if err := recover(); err != nil {
+				ctx.Abort()
+				conf.AppLogger.Error("内部错误: => %#v", err)
 				switch err.(type) {
 				case *errno.BusinessErrorInfo:
 					e := err.(*errno.BusinessErrorInfo)
-					ctx.Abort()
-					ctx.JSON(http.StatusOK, gin.H{
-						"code": e.Code,
-						"msg":  e.Msg,
-						"data": nil,
-					})
+					service.SendResponse(ctx, e, nil)
+				case string, error:
+					service.SendResponse(ctx, errno.InternalServerError, err)
 				default:
-					ee := err.(error)
-					conf.AppLogger.Error(ee.Error())
 					service.SendResponse(ctx, errno.InternalServerError, nil)
 				}
 			}
