c1b7f3f426d083fcb78ca9205b82326dacced88a
pkmm <robotgg@126.com>
2020-02-12 13:56:20 +0800

    format code

--- conf/config.go
+++ conf/config.go
@@ -3,7 +3,6 @@ package conf
 import (
 	"cgin/util"
 	"encoding/json"
-	"fmt"
 	"github.com/astaxie/beego/config"
 	"github.com/astaxie/beego/logs"
 	"path"
@@ -11,7 +10,6 @@ import (
 
 // 使用beego的config包
 var AppConfig config.Configer
-var err error
 
 // 使用beego的log系统
 var Logger *logs.BeeLogger
@@ -24,20 +22,18 @@ const (
 )
 
 func init() {
-	wd := util.GetCurrentCodePath()
-	fmt.Printf("conf path: %#v\n", wd)
-	AppConfig, err = config.NewConfig("ini", path.Join(wd, ".env"))
-	if err != nil {
-		fmt.Println(err)
-		panic("Error of load .env")
-	}
+	// Note: Must init logger module first.
+	Logger = initLogger()
+	AppConfig = initAppConfig()
+}
 
+func initLogger() *logs.BeeLogger {
 	// 设置beego logs
-	Logger = logs.NewLogger(1e5)
-	Logger.SetPrefix("[USE GIN]:")
-	Logger.EnableFuncCallDepth(true)
-	Logger.SetLogFuncCallDepth(10)
-	Logger.Async(1e3)
+	logger := logs.NewLogger(1e5)
+	logger.SetPrefix("[RETAIN]:")
+	logger.EnableFuncCallDepth(true)
+	logger.SetLogFuncCallDepth(10)
+	logger.Async(1e3)
 
 	type logConfig struct {
 		Filename string   `json:"filename"`
@@ -47,13 +43,16 @@ func init() {
 		Separate []string `json:"separate"`
 	}
 
+	wd := util.GetCurrentCodePath()
+	logFileStorageIn :=  path.Join(wd, "..", "storage/logs/gin.log")
 	lf := &logConfig{
-		Filename: path.Join(wd, "..", "storage/logs/gin.log"),
+		Filename: logFileStorageIn,
 		Level:    7,
 		Daily:    true,
 		Maxdays:  2,
 		Separate: []string{"error", "info", "debug"},
 	}
+
 	//`{
 	//"filename":"storage/logs/cgin.log",
 	//"level":7,
@@ -64,6 +63,23 @@ func init() {
 	if byteOfLF, err := json.Marshal(lf); err != nil {
 		panic(err)
 	} else {
-		Logger.SetLogger(logs.AdapterMultiFile, string(byteOfLF))
+		logger.SetLogger(logs.AdapterMultiFile, string(byteOfLF))
 	}
+
+	// print config ini file path.
+	logger.Info("log file storage in: %#v", logFileStorageIn)
+	return logger
+}
+
+func initAppConfig() config.Configer {
+	wd := util.GetCurrentCodePath()
+	confPath := path.Join(wd, ".env")
+	myAppConfig, err := config.NewConfig("ini", confPath)
+	if err != nil {
+		panic(err)
+	}
+	if Logger != nil {
+		Logger.Info("conf path: %#v", confPath)
+	}
+	return myAppConfig
 }
