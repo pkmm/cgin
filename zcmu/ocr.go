2ee4c50d3b15229f9c293648151c7d9833396276
pkmm <robotgg@126.com>
2020-03-30 00:01:27 +0800

    bug fix

--- zcmu/ocr.go
+++ zcmu/ocr.go
@@ -14,43 +14,38 @@ var (
 
 func init() {
 	wd, _ := os.Getwd()
-	modelPath := path.Join(wd, "zf.model")
-	fmt.Println(" === OCR model load in ", modelPath)
+	modelPath := path.Join(wd, "zcmu", "zf.model")
+	fmt.Println("=== OCR model load in ", modelPath)
 	svmModel = libSvm.NewModelFromFile(modelPath)
 }
 
 // recognize verify code.
-func crop(src image.Image, name string) map[string][]float64 {
-	vec := make(map[string][]float64, 0)
-	rgbImg := src
+func picture2vector(picture image.Image) *[][]float64 {
+	vec := make([][]float64, 4) // 4个字符 分为4个向量
 	index := 0
 	for i := 2; i < 50; i += 12 {
-		var tmp []float64
 		for y := 1; y < 22; y++ {
 			for x := 0; x <= 16; x++ {
-				pixel := rgbImg.At(x+i, y)
+				pixel := picture.At(x+i, y)
 				r, g, b, _ := pixel.RGBA()
 				y := (0.3*float64(r) + 0.59*float64(g) + 0.11*float64(b)) / 257.0
-				tmp = append(tmp, y/255.0)
+				vec[index] = append(vec[index], y/255.0)
 			}
 		}
-		vec[fmt.Sprintf("%s-%d", name, index)] = tmp
 		index++
 	}
-	return vec
+	return &vec
 }
 
-func Predict(im image.Image) (string, error) {
-	vec := crop(im, "loc")
-	ret := make([]byte, 0)
-	x := make(map[int]float64)
-	for ind := 0; ind < 4; ind++ {
-		for index, value := range vec[fmt.Sprintf("loc-%d", ind)] {
-			x[index+1] = value
+func Predict(picture image.Image) (string, error) {
+	vector := picture2vector(picture)
+	result := make([]byte, 0)
+	label := make(map[int]float64)
+	for i := 0; i < 4; i++ {
+		for index, value := range (*vector)[i] {
+			label[index+1] = value
 		}
-		predictLabel := svmModel.Predict(x)
-		ans := byte(predictLabel)
-		ret = append(ret, ans)
+		result = append(result, byte(svmModel.Predict(label)))
 	}
-	return string(ret), nil
+	return string(result), nil
 }
