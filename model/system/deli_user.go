703155a8292b0f590a1263aa80b8703600ea43f9
qwesde <robotgg@126.com>
2021-10-11 10:07:34 +0800

    fix bug

--- model/system/deli_user.go
+++ model/system/deli_user.go
@@ -4,7 +4,7 @@ import "cgin/global"
 
 type DeliUser struct {
 	global.GModel
-	Username string `json:"username" gorm:"comment:用户名拼音缩写;uniqueIndex"`
+	Username string `json:"username" gorm:"type:varchar(20);comment:用户名拼音缩写;uniqueIndex:idx_n"`
 	Token    string `json:"token" gorm:"comment:用户得力的token"`
 	Cancel   int    `json:"cancel" gorm:"default:0;comment:不使用自动签到"`
 	Uid      string `json:"uid" gorm:"comment:wxpusher UID"`
